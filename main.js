a8315e17261b47c0841b4d15630d8c751e724343
ძᥱ᥎ᥱᥣ᥆⍴ᥱძ ᑲᥡ • 𝖿ᥱძᥱᥣᥲᥒ <developedfede@gmail.com>
2025-04-13 12:41:07 -0300

    Update main.js

--- main.js
+++ main.js
@@ -128,7 +128,7 @@ const connectionOptions = {
   logger: pino({ level: 'silent' }),
   printQRInTerminal: opcion == '1' ? true : methodCodeQR ? true : false,
   mobile: MethodMobile, 
-  browser: opcion == '1' ? ['Vegeta-Bot-MB', 'Edge', '20.0.04'] : methodCodeQR ? ['Vegeta-Bot-MB', 'Edge', '20.0.04'] : ["Ubuntu", "Chrome", "20.0.04"],
+  browser: opcion == '1' ? ['Vegeta-Bot', 'Edge', '20.0.04'] : methodCodeQR ? ['Vegeta-Bot', 'Edge', '20.0.04'] : ["Ubuntu", "Chrome", "20.0.04"],
   auth: {
     creds: state.creds,
     keys: makeCacheableSignalKeyStore(state.keys, pino({ level: "fatal" }).child({ level: "fatal" })),
@@ -157,17 +157,17 @@ if (!fs.existsSync(`./${sessions}/creds.json`)) {
       if (!!phoneNumber) {
         numeroTelefono = phoneNumber.replace(/[^0-9]/g, '')
         if (!Object.keys(PHONENUMBER_MCC).some(v => numeroTelefono.startsWith(v))) {
-          console.log(chalk.bgBlack(chalk.bold.greenBright(`👻 Por favor, Ingrese el número de WhatsApp.\n${chalk.bold.yellowBright(`👀  Ejemplo: 57321×××××××`)}\n${chalk.bold.magentaBright('---> ')}`)))
+          console.log(chalk.bgBlack(chalk.bold.greenBright(`👻 Por favor, Ingrese el número de WhatsApp.\n${chalk.bold.yellowBright(`👀  Ejemplo: 54911×××××××`)}\n${chalk.bold.magentaBright('---> ')}`)))
           process.exit(0)
         }
       } else {
         while (true) {
-          numeroTelefono = await question(chalk.bgBlack(chalk.bold.greenBright(`👻 Por favor, escriba su número de WhatsApp.\n👀  Ejemplo: 57321×××××××\n`)))
+          numeroTelefono = await question(chalk.bgBlack(chalk.bold.greenBright(`👻 Por favor, escriba su número de WhatsApp.\n👀  Ejemplo: 54911×××××××\n`)))
           numeroTelefono = numeroTelefono.replace(/[^0-9]/g, '')
           if (numeroTelefono.match(/^\d+$/) && Object.keys(PHONENUMBER_MCC).some(v => numeroTelefono.startsWith(v))) {
             break 
           } else {
-            console.log(chalk.bgBlack(chalk.bold.greenBright(`👻 Por favor, escriba su número de WhatsApp.\n  👀Ejemplo: 57321×××××××\n`)))
+            console.log(chalk.bgBlack(chalk.bold.greenBright(`👻 Por favor, escriba su número de WhatsApp.\n  👀Ejemplo: 54911×××××××\n`)))
           }
         }
         rl.close()  
